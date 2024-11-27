15ac9dadace25dddd6feb78a35e4e310187607c2
Recery <142628253+Recery@users.noreply.github.com>
2024-11-26 21:30:33 -0300

    testing auto redeploy

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ ComfyJS.onCommand = (user, activator, msg, flags, extra) => {
     }
 }
 
+
+// probando el redeploy
 ComfyJS.onJoin = (user, self, extra) => {
     if (user === "recerybot") return;
 
