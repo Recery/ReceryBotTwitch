205b3e563ba994baee7a8994b609fb7efed204cd
Recery <142628253+Recery@users.noreply.github.com>
2024-11-28 19:12:10 -0300

    Update Memide.js

--- Scripts/Commands/Memide.js
+++ Scripts/Commands/Memide.js
@@ -1,17 +1,37 @@
-const Command = require("./command_cls.js")
+const DB = require("better-sqlite3");
+const Command = require("./command_cls.js");
 
 class Memide extends Command
 {
     execution(command_content)
     {
+        const db = new DB(process.env.CONTADORES_DB_PATH);
+        
+        let rows = db.prepare("SELECT * FROM promediomemide").all();
+        let add_row = true;
         let tamano = Math.floor((Math.random() * 30) + 1);
+        let promedio = tamano;
 
-        if (tamano <= 10)
-            this.comfy.Say(`A ${command_content.user} le mide ${tamano}cm, tremendo pito corto XD`);
-        else if (tamano <= 20)
-            this.comfy.Say(`A ${command_content.user} le mide ${tamano}cm... `);
+        for (const row of rows)
+        {
+            if (row.username === command_content.user)
+            {
+                add_row = false;
+                
+                promedio = (tamano + row.promedio) / 2;
+                db.prepare("UPDATE promediomemide SET promedio = ? where id = ?").run(promedio, row.id);
+            }
+        }
+
+        if (add_row)
+            db.prepare("INSERT INTO promediomemide (username, promedio) VALUES (?, ?)").run(command_content.user, promedio)
+
+        if (promedio <= 10)
+            this.comfy.Say(`${command_content.user}, te mide ${tamano}cm. En promedio te mide ${promedio}cm, tremendo pito corto XD`);
+        else if (promedio <= 20)
+            this.comfy.Say(`${command_content.user}, te mide ${tamano}cm. En promedio te mide ${promedio}...`);
         else
-            this.comfy.Say(`A ${command_content.user} le mide ${tamano}cm... donde esconde semejante bestia?!`);
+            this.comfy.Say(`${command_content.user}, te mide ${tamano}cm... En promedio te mide ${promedio}cm, donde escondes semejante bestia?!`);
     }
 }
 
