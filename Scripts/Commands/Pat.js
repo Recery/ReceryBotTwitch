bf7ac68f16b65dccc0c472f06104e3477915d96b
Recery <142628253+Recery@users.noreply.github.com>
2024-11-28 17:57:37 -0300

    Update Pat.js

--- Scripts/Commands/Pat.js
+++ Scripts/Commands/Pat.js
@@ -1,10 +1,34 @@
+const DB = require("better-sqlite3")
 const Command = require("./command_cls.js")
 
 class Pat extends Command
 {
     execution(command_content)
     {
-        this.comfy.Say(`${command_content.user} acarició la cabecita de ${command_content.msg}!`);
+        const db = new DB(process.env.CONTADORES_DB_PATH);
+
+        let rows = db.prepare("SELECT * FROM pats").all();
+        let new_pats = 1;
+        let add_row = true;
+
+        for (const row of rows)
+        {
+            if (row.username === command_content.msg)
+            {
+                add_row = false;
+
+                new_pats = row.pats + 1;
+                db.prepare("UPDATE pats SET pats = ? WHERE id = ?").run(new_pats, row.id);
+            }
+        }
+
+        if (add_row)
+            db.prepare("INSERT INTO pats (username, pats) VALUES (?, ?)").run(command_content.msg, new_pats);
+
+        if (new_pats > 1)
+            this.comfy.Say(`${command_content.user} acarició la cabecita de ${command_content.msg} ${new_pats} veces!`);
+        else
+            this.comfy.Say(`${command_content.user} acarició la cabecita de ${command_content.msg} ${new_pats} vez!`);
     }
 }
 
