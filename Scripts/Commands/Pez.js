bb2ed79fadfe11651f55f8042a6c22dcbe5121b3
Recery <142628253+Recery@users.noreply.github.com>
2024-11-28 18:17:05 -0300

    Update Pez.js

--- Scripts/Commands/Pez.js
+++ Scripts/Commands/Pez.js
@@ -1,10 +1,34 @@
+const DB = require("better-sqlite3")
 const Command = require("./command_cls.js")
 
 class Pez extends Command
 {
     execution(command_content)
     {
-        this.comfy.Say(`${command_content.user} es un ${Math.floor(Math.random() * 100 + 1)}% pez`);
+        const db = new DB(process.env.CONTADORES_DB_PATH);
+
+        let rows = db.prepare("SELECT * FROM promediopez").all();
+
+        let add_row = true;
+        let porcentaje = Math.floor(Math.random() * 100 + 1);
+        let promedio = porcentaje;
+
+        for (const row of rows)
+        {
+            if (row.username === command_content.msg)
+            {
+                add_row = false;
+
+                promedio = (porcentaje + row.promedio) / 2;
+                db.prepare("UPDATE promediopez SET promedio = ? WHERE id = ?").run(promedio, row.id);
+            }
+        }
+
+        if (add_row)
+            db.prepare("INSERT INTO promediopez (username, promedio)").run(command_content.msg, promedio);
+
+        this.comfy.Say(`${command_content.user}, eres un ${porcentaje}% pez. 
+            En promedio eres un ${promedio} pez.`);
     }
 }
 
