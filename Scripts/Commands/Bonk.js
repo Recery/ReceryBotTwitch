1f92134cc9e6df020bfd4e2e84e04dddb5eeb758
Recery <142628253+Recery@users.noreply.github.com>
2024-11-28 17:34:24 -0300

    Update Bonk.js

--- Scripts/Commands/Bonk.js
+++ Scripts/Commands/Bonk.js
@@ -1,10 +1,36 @@
+const DB = require("better-sqlite3")
 const Command = require("./command_cls.js")
 
 class Bonk extends Command
 {
     execution(command_content)
     {
-        this.comfy.Say(`${command_content.user} bonkeó a ${command_content.msg} XD`);
+        const db = new DB(process.env.CONTADORES_DB_PATH);
+
+        let rows = db.prepare("SELECT * FROM bonks").all();
+        let new_bonks = 1;
+        let add_row = true;
+
+        for (const row of rows)
+        {
+            if (row.username === command_content.msg)
+            {
+                add_row = false;
+
+                new_bonks = row.bonks + 1;
+                db.prepare("UPDATE bonks SET bonks = ? WHERE id = ?").run(new_bonks, row.id);
+            }
+        }
+
+        if (add_row) // No hay datos de antes de este usuario, hay que agregarlos...
+        {
+            db.prepare("INSERT INTO bonks (username, bonks) VALUES (?, ?)").run(command_content.msg, new_bonks);
+        }
+
+        if (new_bonks > 1)
+            this.comfy.Say(`${command_content.user} bonkeó a ${command_content.msg} ${new_bonks} vez XD`);
+        else
+            this.comfy.Say(`${command_content.user} bonkeó a ${command_content.msg} ${new_bonks} veces XD`);
     }
 }
 
