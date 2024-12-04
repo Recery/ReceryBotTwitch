59ffd004262745c28c553789cc6421a7b89f3522
Recery <142628253+Recery@users.noreply.github.com>
2024-12-03 21:24:26 -0300

    Create Translate.js

---
+++ Scripts/Commands/Translate.js
@@ -0,0 +1,27 @@
+const Command = require("./command_cls.js")
+
+const fetch = require("node-fetch")
+const libretranslate_url = 'https://libretranslate.com';
+
+class Translate extends Command
+{
+    async execution(command_content)
+    {
+        const response = await fetch(`${libretranslate_url}/translate`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                q: command_content.msg,
+                source: 'en',
+                target: 'es',
+                source: 'text',
+            }),
+        });
+
+        const data = await response.json();
+
+        this.comfy.Say(`${data.translatedText}`);
+    }
+}
+
+module.exports = new Translate("t");
