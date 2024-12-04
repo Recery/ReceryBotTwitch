2b9200932bb1f3d9a77d59e4c503b36c1fa17032
Recery <142628253+Recery@users.noreply.github.com>
2024-12-03 22:10:22 -0300

    Update Translate.js

--- Scripts/Commands/Translate.js
+++ Scripts/Commands/Translate.js
@@ -5,10 +5,22 @@ const libretranslate_url = 'https://libretranslate.com';
 
 class Translate extends Command
 {
+    valid_langs = ['en', 'es'];
+
     async execution(command_content)
     {
+        const args = command_content.msg.split(' ');
+        const target_lang = args[0];
+        const text_to_translate = args.slice(1).join(' ');
+
+        if (!this.valid_langs.includes(target_lang))
+        {
+            this.comfy.Say("Please choose a valid language.");
+            return;
+        }
+
         try {
-            const response = await Translatte(command_content.msg, {to: 'es'});
+            const response = await Translatte(text_to_translate, {to: target_lang});
 
             if (response && response.text) this.comfy.Say(`${response.text}`);
             else this.comfy.Say("No se pudo traducir.");
